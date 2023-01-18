149941ff8a8a311cf76237311084c67e656d1caf
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-01-18 15:14:32 +0100

    Develop (#2)
    * [UPDATE] : add responsive for mobile
    
    * [UPDATE] : add mobile responsive
    
    * [ADD] : add team section
    
    Co-authored-by: CyriacduChatenet <cyriacduchatenet@icloud.com>

---
+++ src/components/Team.jsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import podium from "../assets/podium.png";
+import { getTopThreeUser } from "../services/services/user.service";
+import screen1 from "../assets/screen1.jpg";
+import screen2 from "../assets/screen2.jpg";
+import screen3 from "../assets/screen3.jpg";
+import screen4 from "../assets/screen4.jpg";
+
+const Team = () => {
+  const [lastUsers, setLastUsers] = useState([]);
+
+  useEffect(() => {
+    getTopThreeUser(setLastUsers);
+    // getLastUsers();
+    if (lastUsers) {
+      console.log(lastUsers);
+    }
+  }, []);
+
+  return (
+    <>
+      {lastUsers ? (
+        <div className="team-container">
+          <h2 className="rank-title">Les équipes</h2>
+          <div className="creations-container">
+            <h4>Vos créations</h4>
+            <div className="slider">
+              <img src={screen1} alt="" className="imgScreen" />
+              <img src={screen2} alt="" className="imgScreen" />
+              <img src={screen3} alt="" className="imgScreen" />
+              <img src={screen4} alt="" className="imgScreen" />
+            </div>
+          </div>
+          <div className="podium-container">
+            <div className="podium-left">
+              <div className="podium-user">
+                {lastUsers.map((user, index) => (
+                  <p key={index} className="userNumber">
+                    {user.username}
+                  </p>
+                ))}
+              </div>
+              <img src={podium} alt="" />
+              <div className="podium-shadow"></div>
+            </div>
+            <div className="podium-right">
+              <div className="podium-text">
+                <p>
+                  Si vous atteignez le nombre de 10 coins alors vous êtes
+                  éligibles au tirage au sort pour gagner des Airpods troisième
+                  génération.{" "}
+                </p>
+                <br /> <br />{" "}
+                <p>
+                  À chaque 10 coins obtenus, votre chance d’être tiré au sort
+                  est multiplié.
+                </p>
+                <br /> <br />{" "}
+                <p>
+                  Les trois personnes qui auront posés le plus de pixels
+                  gagneront des goodies !
+                </p>{" "}
+                <br /> <br />{" "}
+                <p>
+                  La collecte de points se fait sur toute la semaine ! Mais
+                  jeudi soir, le nombre de points seront doublés.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
+};
+
+export default Team;
--- src/services/services/user.service.js
+++ src/services/services/user.service.js
@@ -13,8 +13,26 @@ const getTopUser = async (setTopUsers) => {
             }
             return user.data()
         }).sort((a, b) => b.totalScore - a.totalScore).splice(0, NUMBER_USER)
-        console.log(topUsers);
+        // console.log(topUsers);
         setTopUsers(topUsers);
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+const getTopThreeUser = async (setTopUsers) => {
+    const NUMBER_USER = 3
+    try {
+        const users = await getDocs(userCollection)
+        const topUsers = users.docs.map(user => {
+            if(user.data().email === 'admin@admin.com') {
+                return user.totalScore = 0
+            }
+            return user.data()
+        }).sort((a, b) => b.totalScore - a.totalScore).splice(0, NUMBER_USER)
+        // console.log(topUsers);
+        const arr = [topUsers[1], topUsers[0], topUsers[2]]
+        setTopUsers(arr);
     } catch (error) {
         throw new Error(error)
     }
@@ -35,5 +53,6 @@ const listenTopUser = async (setTopUsers) => {
 
 export {
     getTopUser,
+    getTopThreeUser,
     listenTopUser,
 }
